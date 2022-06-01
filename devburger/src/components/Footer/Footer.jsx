af3b3a1e0d8a275d70df6f05d349bceccd9f4007
César Freitas <cesarfreitascontato@gmail.com>
2022-06-01 11:40:22 -0300

    feat: add div widgetFooter no componente Footer

--- devburger/src/components/Footer/Footer.jsx
+++ devburger/src/components/Footer/Footer.jsx
@@ -1,17 +1,48 @@
 import style from './Footer.module.css'
+import { FaInstagramSquare, FaFacebookSquare, FaTwitterSquare, FaAndroid, FaApple } from 'react-icons/fa'
 
 const Footer = (props) => {
     return (
         <div className={style.footer}>
-            <p>
-                <span className={style.rights}>Dev Burguer, 2022 - Todos os direitos reservados.</span>
-                <br></br>
-                <br></br>
-                <span>
-                    Projeto realizado com fins educacionais por: <strong><a href='https://www.linkedin.com/in/annanda-santos-a93196142/'>Annanda</a></strong>, <strong><a href='https://www.linkedin.com/in/cesarfreitascontato/'>César</a></strong>, <strong><a href='https://www.linkedin.com/in/eduardo-fortes-a3a024a4/'>Eduardo</a></strong> e <strong><a href='https://www.linkedin.com/in/raphhael-m/'>Raphael</a></strong>.
-                </span>
-            </p>
+
+            <div className={style.widgetFooter}>
+                <div className={style.mapaDoSite}>
+                    <h3>Mapa do Site</h3>
+                    <a href='#'>Home</a>
+                    <a href="#">Cardápio</a>
+                    <a href="#">Sobre Nós</a>
+                </div>
+                <div className={style.sigaNos}>
+                    <h3>Dev Burger</h3>
+                    <p>Siga nossas redes</p>
+                    <div className={style.redes}>
+                        <a href='#'><FaInstagramSquare size='1.8rem' color='#EDA83B'/></a>
+                        <a href="#"><FaFacebookSquare size='1.8rem' color='#EDA83B'/></a>
+                        <a href="#"><FaTwitterSquare size='1.8rem' color='#EDA83B'/></a>
+                    </div>
+                </div>
+                <div className={style.baixeNossoApp}>
+                    <h3>Baixe nosso App</h3>
+                    <div className={style.plataformas}>
+                    <a href="#"><FaAndroid size='3rem' color='#EDA83B'/></a>
+                    <a href="#"><FaApple size='3rem' color='#EDA83B'/></a>
+                    </div>
+                </div>
+                <div className={style.logo}>
+                    <img src="./src/logo/DevBurger_logoFinal.png" alt="some text" height={200} width={200} ></img>
+                </div>
+            </div>
+
+            <div className={style.rightsContainer}>
+                <p>
+                    <span className={style.rights}>Dev Burguer, 2022 - Todos os direitos reservados.</span>
+                    <br></br>
+                    <br></br>
+                    <span className={style.infos}><strong>RAZÃO SOCIAL:</strong> FRANQUEADORA DEV BURGUER LTDA | <strong>TELEFONE:</strong> (11) 1111-1111 | <strong>CNPJ:</strong> 22.222.222/2222-22 | <strong>ENDEREÇO:</strong> RUA, XXXX - ESTADO <br></br>(BAIRRO) - CEP: 33333-333</span>
+                </p>
+            </div>
         </div>
+
     )
 }
 
