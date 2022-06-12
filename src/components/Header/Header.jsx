4ba5c02c14dc4fec4d456433c72fbec7065cd326
Raphael Marques <rmarquescomputadores@gmail.com>
2022-06-12 17:59:12 -0300

    Ajust Nav
    Ajuste Links no NAV

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
         <div className={styles.nav}>
             <ul className={styles.ul}> 
                 < Link to ="/"><li> Home </li></Link>
-                < Link to="/cardapio"><li> Cardápio </li></Link>
+                < Link to="/cardapio"><li>Cardápio </li></Link>
                 < Link to="/sobre"><li>Sobre Nós</li></Link>
             </ul>
         </div>
